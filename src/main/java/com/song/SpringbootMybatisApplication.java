98937bfc90e9a826e8818f289e45b6c561626748
forversdream <1277668914@qq.com>
2022-10-25 20:42:10 +0800

    first commit

--- src/main/java/com/song/SpringbootMybatisApplication.java
+++ src/main/java/com/song/SpringbootMybatisApplication.java
@@ -11,5 +11,5 @@ public class SpringbootMybatisApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMybatisApplication.class, args);
     }
-
+//TEST
 }
